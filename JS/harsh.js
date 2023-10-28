29391e70d711b10b79b4500116f5c8fce5f4fd6f
Harsh Lakhanpal <harsh.lakhanpal3@gmail.com>
2023-10-28 23:55:08 +0530

    Portfolio

--- JS/harsh.js
+++ JS/harsh.js
@@ -5,3 +5,41 @@ var typed = new Typed(".autoType", {
   backSpeed: 150,
   loop: true,
 });
+
+// for mouse move background
+var follow = document.querySelector(".follow");
+
+var body = document.querySelector(".fullDiv");
+
+body.addEventListener("mousemove", function (dets) {
+  follow.style.visibility = "visible";
+  const height = follow.offsetHeight;
+  const width = follow.offsetWidth;
+
+  setTimeout(() => {
+    follow.style.top = `${dets.pageY - height / 2}px`;
+    follow.style.left = `${dets.pageX - width / 2}px`;
+  }, 20);
+
+  // body.style.backgroundColor = "red"
+});
+
+body.addEventListener("mouseleave", function () {
+  follow.style.visibility = "hidden";
+});
+
+// pdf print
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get a reference to the PDF link element
+  var pdfLink = document.getElementById(".pdfLink");
+
+  // Add a click event listener to the link
+  pdfLink.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the default link behavior (navigating to the PDF file)
+
+    // Open the PDF in a new window or tab
+    var pdfUrl = pdfLink.getAttribute("href");
+    // window.open(pdfUrl, "_blank");
+  });
+});
